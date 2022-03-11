7f4ce8e5cb5344228034a19ae7306d3629e73f2b
Petter Carlsson <petter.carlsson@afry.com>
2022-03-11 14:48:40 +0100

    AB#60 - Added AccountsController plus subsequent views

---
+++ MoneyPortalMain/Controllers/AccountsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyPortalMain.Controllers
+{
+    public class AccountsController : Controller
+    {
+        [HttpGet("[controller]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("[controller]/[action]/{accountName}")]
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpPost("[controller]/[action]")]
+        public IActionResult Create()
+        {
+            return Ok("Create new savings/checking/cash/credit card account");
+        }
+    }
+}
