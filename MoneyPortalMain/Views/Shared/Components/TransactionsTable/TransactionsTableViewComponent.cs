b84ff7c116e0a0a85bd0734c20b453d4f90d9c2c
Petter Carlsson <petter.carlsson@afry.com>
2022-03-30 15:34:59 +0200

    AB#119 - Created ViewComponent for transactions table

---
+++ MoneyPortalMain/Views/Shared/Components/TransactionsTable/TransactionsTableViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyPortalMain.Models;
+
+namespace MoneyPortalMain.Views.Shared.Components.TransactionsTable
+{
+    public class TransactionsTableViewComponent : ViewComponent
+    {
+
+        public TransactionsTableViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(List<Transaction> transactionsList)
+        {
+            return await Task.FromResult((IViewComponentResult)View("TransactionsTable", transactionsList));
+        }
+    }
+}
