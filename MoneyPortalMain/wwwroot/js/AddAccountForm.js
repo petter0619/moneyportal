badbc17c9fcfe5a9b2947388c324d9c97d5aab8d
petter0619 <petter0619@gmail.com>
2022-04-05 11:12:41 +0200

    AB#128 - Added popup for Add Account form

---
+++ MoneyPortalMain/wwwroot/js/AddAccountForm.js
@@ -0,0 +1,15 @@
+﻿const addAccountFormPopup = document.querySelector('.addAccountPopup');
+const addAccountFormPopupTriggers = document.querySelectorAll('.addAccountPopup__trigger');
+
+const handleShowPopup = e => {
+    const isOpen = addAccountFormPopup.classList.contains('addAccountPopup--open');
+
+    if (isOpen) {
+        addAccountFormPopup.classList.remove('addAccountPopup--open');
+    } else {
+        addAccountFormPopup.classList.add('addAccountPopup--open');
+    }
+}
+
+
+addAccountFormPopupTriggers.forEach(trigger => trigger.addEventListener('click', handleShowPopup));
